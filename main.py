bcd71a103f8a764ba24ea0dc6f53d839143f049b
Lucas Maia <lucas.mdantas@icloud.com>
2024-01-26 11:25:01 -0300

    criando texto personalizado do cartao digital

--- main.py
+++ main.py
@@ -12,6 +12,13 @@ def send_birthday_email(lista_emails):
         email.To = email_address
         #email.Send()
 
+def formatar_texto_cartao(df_aniversariantes):
+    for i, linha in df_aniversariantes.iterrows():
+        nome_completo = linha['NOME'].split()
+        nome_formatado = ' '.join([nome_completo[0], nome_completo[-1]])
+        setor = linha['SETOR']
+        texto_cartao = ' - '.join([nome_formatado, setor])
+        return texto_cartao
 
 df = pd.read_pickle('aniversarios.pkl')
 hoje = datetime.now().date()
@@ -26,3 +33,6 @@ print(aniversariantes)
 if not aniversariantes.empty:
     lista_emails = df['EMAIL'].tolist()
     send_birthday_email(lista_emails)
+
+texto_cartao = formatar_texto_cartao(aniversariantes)
+print(texto_cartao)
