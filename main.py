1c44b8b4f5c10fab350e4169439475d59baddb8a
Lucas Maia <lucas.mdantas@icloud.com>
2024-01-26 09:57:05 -0300

    adicao de coluna de email no dataframe

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import pandas as pd
 from datetime import datetime
 
+def send_email(aniversariantes):
+    pass
+
 df = pd.read_pickle('aniversarios.pkl')
 hoje = datetime.now().date()
 
+df['EMAIL'] = [f'colaborador{i}@gmail.com' for i in range( len(df) )]
+
+print(df)
+
+df.iloc[5,1] = datetime.now().date()
+
 aniversariantes = df[ df['DATA'] == hoje]
 print(aniversariantes)
+
+if not aniversariantes.empty:
+    pass
