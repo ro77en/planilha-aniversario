68f773cd6bcd09642b945fb3049b7c313e4df026
Lucas Maia <lucas.mdantas@icloud.com>
2024-01-26 10:23:16 -0300

    comeco da funcao de envio de emails

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import pandas as pd
 from datetime import datetime
+import win32com.client as win32
 
-def send_email(lista_emails):
-    pass
+def send_birthday_email(lista_emails):
+    outlook = win32.Dispatch('outlook.application')
+
+    for email_address in lista_emails:
+        email = outlook.CreateItem(0)
+        email.Subject = 'Hoje tem Festa!'
+        email.HTMLBody = """ """
+        email.To = email_address
+        #email.Send()
+
 
 df = pd.read_pickle('aniversarios.pkl')
 hoje = datetime.now().date()
@@ -11,11 +20,11 @@ df['EMAIL'] = [f'colaborador{i}@gmail.com' for i in range( len(df) )]
 
 print(df)
 
-df.iloc[5,1] = datetime.now().date()
+df.iloc[5,1] = hoje
 
 aniversariantes = df[ df['DATA'] == hoje]
 print(aniversariantes)
 
 if not aniversariantes.empty:
-    lista_emails = aniversariantes['EMAIL'].tolist()
-    send_email(lista_emails)
+    lista_emails = df['EMAIL'].tolist()
+    send_birthday_email(lista_emails)
